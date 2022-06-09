432a012c95032a3d04511654d74904c823534710
sabankocal <sabankocal@gmail.com>
2022-06-09 22:55:50 +0200

    Update Lesson_06.c

---
+++ Lesson_06/Lesson_06.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("sizeof char=%lu\n", sizeof(char));
+	printf("sizeof short=%lu\n", sizeof(short));
+	printf("sizeof int=%lu\n", sizeof(int));
+	printf("sizeof long=%lu\n", sizeof(long));
+	printf("sizeof long long=%lu\n", sizeof(long long));
+	
+	return 0;
+}
